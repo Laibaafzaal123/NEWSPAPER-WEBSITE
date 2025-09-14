aafb551413db2c75d3cad0cb946e71225700b1a9
Laibaafzaal123 <laibafzaal779@gmail.com>
2025-09-14 09:25:52 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+function toggleText(cardNumber) { 
+  let dots = document.getElementById("dots" + cardNumber);
+  let moreText = document.getElementById("more" + cardNumber);
+  let btn = document.getElementById("btn" + cardNumber);
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btn.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btn.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+  
